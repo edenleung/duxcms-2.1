c5540bf410eb75a3efbd9d716437ef098af843bf
xiaodit <saybabi@163.com>
2019-10-19 17:21:55 +0800

    添加 个性化rewrite demo

--- data/config/rewrite.php
+++ data/config/rewrite.php
@@ -10,5 +10,15 @@ return [
         'form-<name>.html'          => 'duxcms/Form/index',
         'tags-list/<name>.html'     => 'duxcms/Tags/index',
         'tags/<name>.html'          => 'duxcms/TagsContent/index',
+
+        /*
+            个性化自定义
+            // 列表页
+            '<urlname>/index.html'      => 'article/Category/index',
+            // 列表详情页
+            '<class_urlname>/<urltitle>.html' => 'article/Content/index',
+            // 单页面
+            '<urlname>'      => 'page/Category/index',
+        */
     ],
 ];
